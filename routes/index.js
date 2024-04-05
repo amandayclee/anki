e9d05f6c1332c92f6010c3d262e82466c063f968
Yi Chien Lee <yician.lee@gmail.com>
2024-04-05 13:56:29 -0700

    add basic css for home page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+  res.render('home', { title: 'All Decks' });
 });
 
 // Google OAuth login route
