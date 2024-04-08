967dfdde5622516ca4157054f1751ea5250cf691
Yi Chien Lee <yician.lee@gmail.com>
2024-04-08 12:39:25 -0700

    add add a card page

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,10 +1,16 @@
 const Card = require('../models/card');
 
 module.exports = {
-    index
-}
+    index,
+    new: newCard
+};
+
 
 async function index(req, res) {
-    const cards = await Card.find({});
+    // const cards = await Card.find({});
     res.render('cards/index', {title: 'All Cards'});
+}
+
+async function newCard(req, res) {
+    res.render('cards/new', {title: 'Add a Card'});
 }
--- routes/cards.js
+++ routes/cards.js
@@ -5,7 +5,7 @@ var cardsCtrl = require('../controllers/cards')
 // GET /cards/
 router.get('/', cardsCtrl.index);
 // GET /cards/new
-// router.get('/new', cardsCtrl.new);
+router.get('/new', cardsCtrl.new);
 
 router.get('/test', function(req, res, next) {
     res.send('respond with a resource');
